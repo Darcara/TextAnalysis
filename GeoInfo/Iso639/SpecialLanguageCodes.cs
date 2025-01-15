8c2585ce567fcc7c45951ce77ee647a3944dd580
Darcara <830228+Darcara@users.noreply.github.com>
2025-01-15 01:55:37 +0100

    Readme and known special language codes added

---
+++ GeoInfo/Iso639/SpecialLanguageCodes.cs
@@ -0,0 +1,12 @@
+﻿namespace GeoInfo.Iso639;
+
+public static class SpecialLanguageCodes {
+	/// <inheritdoc cref="Language.Uncoded_languages"/> 
+	public const Language Uncoded = Language.Uncoded_languages;
+	/// <inheritdoc cref="Language.Multiple_languages"/> 
+	public const Language Multiple = Language.Multiple_languages;
+	/// <inheritdoc cref="Language.Undetermined"/> 
+	public const Language Undetermined = Language.Undetermined;
+	/// <inheritdoc cref="Language.No_linguistic_content"/> 
+	public const Language NoLinguisticContent = Language.No_linguistic_content;
+}
---
+++ GeoInfo/README.md
@@ -0,0 +1,17 @@
+﻿Enums for ISO languages, countries and currencies
+
+# ISO639 Language
+
+```csharp
+Language l = Language.English;
+// Every Language has a 639-3 3 code
+String iso639_3Code = Language.English.Get3Code();  // eng
+String iso639_2Code = Language.English.Get2Code();  // en
+
+Language english = LanguageHelper.GetLanguageBy2Code("en");
+Language english = LanguageHelper.GetLanguageBy2Code("en-us");
+Language english = LanguageHelper.GetLanguageBy3Code("eng");
+```
+
+# ISO3166 Countries
+# ISO427 Currency
