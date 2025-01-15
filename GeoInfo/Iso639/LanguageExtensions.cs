a604491fc0c0a51b6b0ccbc6531c7006b36646a7
Darcara <830228+Darcara@users.noreply.github.com>
2025-01-15 01:56:08 +0100

    Extended chard-codes to language with bytes to language

--- GeoInfo/Iso639/LanguageExtensions.cs
+++ GeoInfo/Iso639/LanguageExtensions.cs
@@ -1,18 +1,16 @@
 ﻿namespace GeoInfo.Iso639;
 
 public static class LanguageExtensions {
-
-
 	public static Boolean Has2Code(this Language language) {
 		if (!Enum.IsDefined(language) || language == Language.Uninitialized) return false;
-		UInt32 value = ((UInt32)language)&0xFFFF0000;
+		UInt32 value = ((UInt32)language) & 0xFFFF0000;
 		if (value == 0x0000) return false;
 		return true;
 	}
 
 	public static String Get2Code(this Language language) {
 		if (!Enum.IsDefined(language) || language == Language.Uninitialized) return LanguageHelper.Unavailable2;
-		UInt32 value = ((UInt32)language)&0xFFFF0000;
+		UInt32 value = ((UInt32)language) & 0xFFFF0000;
 		if (value == 0x0000) return LanguageHelper.Unavailable2;
 		--value;
 		Span<Char> chars = stackalloc Char[2];
@@ -21,13 +19,42 @@ public static class LanguageExtensions {
 		return new String(chars);
 	}
 
+	internal static void Get2CodeBytes(this Language language, Span<Byte> bytes) {
+		if (!Enum.IsDefined(language) || language == Language.Uninitialized) {
+			LanguageHelper.Unavailable2Bytes.CopyTo(bytes);
+			return;
+		}
+
+		UInt32 value = ((UInt32)language) & 0xFFFF0000;
+		if (value == 0x0000) {
+			LanguageHelper.Unavailable2Bytes.CopyTo(bytes);
+			return;
+		}
+
+		--value;
+		bytes[0] = (Byte)((Byte)'a' + ((value >> 17) & 0b11111));
+		bytes[1] = (Byte)((Byte)'a' + ((value >> 22) & 0b11111));
+	}
+
 	public static String Get3Code(this Language language) {
 		if (!Enum.IsDefined(language) || language == Language.Uninitialized) return LanguageHelper.Unavailable3;
-		Int32 value = (Int32)language -1;
+		Int32 value = (Int32)language - 1;
 		Span<Char> chars = stackalloc Char[3];
 		chars[0] = (Char)((Byte)'a' + ((value >> 1) & 0b11111));
 		chars[1] = (Char)((Byte)'a' + ((value >> 6) & 0b11111));
 		chars[2] = (Char)((Byte)'a' + ((value >> 11) & 0b11111));
 		return new String(chars);
+	}
+
+	public static void Get3CodeBytes(this Language language, Span<Byte> bytes) {
+		if (!Enum.IsDefined(language) || language == Language.Uninitialized) {
+			LanguageHelper.Unavailable3Bytes.CopyTo(bytes);
+			return;
+		}
+
+		Int32 value = (Int32)language - 1;
+		bytes[0] = (Byte)((Byte)'a' + ((value >> 1) & 0b11111));
+		bytes[1] = (Byte)((Byte)'a' + ((value >> 6) & 0b11111));
+		bytes[2] = (Byte)((Byte)'a' + ((value >> 11) & 0b11111));
 	}
 }
--- GeoInfo/Iso639/LanguageHelper.cs
+++ GeoInfo/Iso639/LanguageHelper.cs
@@ -4,11 +4,13 @@ using System.Collections.Frozen;
 
 public static class LanguageHelper {
 	public const String Unavailable2 = "??";
+	internal static ReadOnlySpan<Byte> Unavailable2Bytes => "??"u8;
 	public const String Unavailable3 = "???";
+	internal static ReadOnlySpan<Byte> Unavailable3Bytes => "???"u8;
 
 	public static FrozenDictionary<String, Language> CreateFast2CodeLookup() => Enum.GetValues<Language>().Where(l => l.Has2Code()).ToFrozenDictionary(l => l.Get2Code(), l => l, StringComparer.OrdinalIgnoreCase);
 	public static FrozenDictionary<String, Language> CreateFast3CodeLookup() => Enum.GetValues<Language>().Where(l => l > Language.Uninitialized).ToFrozenDictionary(l => l.Get3Code(), l => l, StringComparer.OrdinalIgnoreCase);
-
+	
 	public static Language GetLanguageByCode(ReadOnlySpan<Char> languageCode) {
 		if (languageCode.Length < 2) return Language.Undetermined;
 		if (languageCode.Length == 2) return GetLanguageBy2Code(languageCode);
@@ -17,13 +19,42 @@ public static class LanguageHelper {
 		if (languageCode[2] == '-' || languageCode[2] == '_') return GetLanguageBy2Code(languageCode.Slice(0, 2));
 		return Language.Undetermined;
 	}
+	
+	public static Language GetLanguageByCode(ReadOnlySpan<Byte> languageCode) {
+		if (languageCode.Length < 2) return Language.Undetermined;
+		if (languageCode.Length == 2) return GetLanguageBy2Code(languageCode);
+		if (languageCode.Length == 3) return GetLanguageBy3Code(languageCode);
 
+		if (languageCode[2] == '-' || languageCode[2] == '_') return GetLanguageBy2Code(languageCode.Slice(0, 2));
+		return Language.Undetermined;
+	}
+
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="language2Code">ISO 639-1 language code (2 letters)</param>
+	/// <returns></returns>
 	public static Language GetLanguageBy2Code(ReadOnlySpan<Char> language2Code) {
 		if(language2Code.Length != 2 || language2Code.SequenceEqual(Unavailable2)) return Language.Undetermined;
 		Span<Char> lower = stackalloc Char[2];
 		language2Code.ToLowerInvariant(lower);
 		foreach (Language language in Enum.GetValues<Language>()) {
 			if(lower.SequenceEqual(language.Get2Code())) return language;
+		}
+		return Language.Undetermined;
+	}
+	
+	public static Language GetLanguageBy2Code(ReadOnlySpan<Byte> language2Code) {
+		if(language2Code.Length != 2 || language2Code.SequenceEqual(Unavailable2Bytes)) return Language.Undetermined;
+		Span<Byte> buffer = stackalloc Byte[4];
+		Span<Byte> lower = buffer.Slice(0, 2);
+		lower[0] = (Byte)(language2Code[0] < 97 ? language2Code[0] + 32 : language2Code[0]);
+		lower[1] = (Byte)(language2Code[1] < 97 ? language2Code[1] + 32 : language2Code[1]);
+		Span<Byte> codeBuffer = buffer.Slice( 2);
+		foreach (Language language in Enum.GetValues<Language>()) {
+			language.Get2CodeBytes(codeBuffer);
+			if(lower.SequenceEqual(codeBuffer)) return language;
 		}
 		return Language.Undetermined;
 	}
@@ -40,6 +71,21 @@ public static class LanguageHelper {
 		language3Code.ToLowerInvariant(lower);
 		foreach (Language language in Enum.GetValues<Language>()) {
 			if(lower.SequenceEqual(language.Get3Code())) return language;
+		}
+		return Language.Undetermined;
+	}
+	
+	public static Language GetLanguageBy3Code(ReadOnlySpan<Byte> language3Code) {
+		if(language3Code.Length != 3 || language3Code.SequenceEqual(Unavailable3Bytes)) return Language.Undetermined;
+		Span<Byte> buffer = stackalloc Byte[6];
+		Span<Byte> lower = buffer.Slice(0, 3);
+		lower[0] = (Byte)(language3Code[0] < 97 ? language3Code[0] + 32 : language3Code[0]);
+		lower[1] = (Byte)(language3Code[1] < 97 ? language3Code[1] + 32 : language3Code[1]);
+		lower[2] = (Byte)(language3Code[2] < 97 ? language3Code[2] + 32 : language3Code[2]);
+		Span<Byte> codeBuffer = buffer.Slice( 3);
+		foreach (Language language in Enum.GetValues<Language>()) {
+			language.Get3CodeBytes(codeBuffer);
+			if(lower.SequenceEqual(codeBuffer)) return language;
 		}
 		return Language.Undetermined;
 	}
