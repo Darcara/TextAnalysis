8e04f7f548c65b0348d53c6bf1864d2c22ca5604
Darcara <830228+Darcara@users.noreply.github.com>
2025-02-19 00:13:48 +0100

    Missing project file added

---
+++ GeoInfo/GeoDataInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace GeoInfo;
+
+using System.Globalization;
+
+public static class GeoDataInfo {
+	public static readonly DateTime LastUpdated = DateTime.Parse("2025-02-19T12:00:00", DateTimeFormatInfo.InvariantInfo, DateTimeStyles.AssumeUniversal | DateTimeStyles.NoCurrentDateDefault);
+}
